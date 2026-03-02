6acb3854f3caf774cb3c73e3c2feedba430390b9
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2026-03-02 15:07:08 +0300

    Upd

--- src/main/kotlin/Attachment.kt
+++ src/main/kotlin/Attachment.kt
@@ -29,17 +29,44 @@ data class Audio(
     var duration: Int
 )
 
+data class File(
+    var id: Int,
+    var ownerId: Int,
+    var title: String,
+    var size: Int,
+    var ext: String
+)
+
+data class Graffiti(
+    var id: Int,
+    var ownerId: Int,
+    var url: String,
+    var width: Int,
+    var height: Int
+)
+
+
 data class PhotoAttachment(
-    override val type: String = "Photo",
+    override val type: String = "photo",
     val photo: Photo
 ) : Attachment
 
 data class VideoAttachment(
-    override val type: String = "Video",
+    override val type: String = "video",
     val video: Video
 ) : Attachment
 
 data class AudioAttachment(
-    override val type: String = "Audio",
+    override val type: String = "audio",
     val audio: Audio
+) : Attachment
+
+data class FileAttachment(
+    override val type: String = "file",
+    val file: File
+) : Attachment
+
+data class GraffitiAttachment(
+    override val type: String = "graffiti",
+    val graffiti: Graffiti
 ) : Attachment
